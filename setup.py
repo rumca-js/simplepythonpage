dc18817320de0a16d1c0f03050b36ef9f2184daf
X Y Z <rozbujnik@gmail.com>
2022-02-22 12:51:05 +0100

    Added dummy test

--- setup.py
+++ setup.py
@@ -18,5 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    test_suite="simplepythonpage.tests.test_all",
 )
---
+++ simplepythonpage/tests/test_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: <encoding name> -*-
+
+import unittest
+import logging
+
+
+class TestPyMarkDownSax(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_title(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
